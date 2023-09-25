ae7c1cc9b92b5790b78094de1489aee7c30db1c7
Daniel_Sparky <45275543+Thekillergod41@users.noreply.github.com>
2023-09-25 22:22:20 +1000

    Delete Aussieskys directory

--- Aussieskys/dbo.TableTimetable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[TableTimetable] (
-    [title]       NCHAR (10) NOT NULL,
-    [description] NCHAR (10) NOT NULL,
-    [day]         NCHAR (10) NOT NULL,
-    [start_time]  TIME NOT NULL,
-    [end_time]    TIME NOT NULL
-);
-
